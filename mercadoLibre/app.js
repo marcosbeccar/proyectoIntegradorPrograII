7a65721fdf7cf9a5effecf25ce0c27728dce637d
marcosbeccar <beccarvarelam@udesa.edu.ar>
2024-06-10 17:24:22 -0300

    session

--- mercadoLibre/app.js
+++ mercadoLibre/app.js
@@ -24,7 +24,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(session( { 
-  secret: "Nuestro mensaje secreto",
+  secret: "proyectointegrador2",
 	resave: false,
 	saveUninitialized: true 
 }));
