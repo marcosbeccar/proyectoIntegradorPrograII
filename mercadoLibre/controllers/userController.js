787f4544db34ea682ad52b0d40f9f1624dc616aa
marcosbeccar <beccarvarelam@udesa.edu.ar>
2024-06-12 23:39:47 -0300

    validaciones completas de Register

--- mercadoLibre/controllers/userController.js
+++ mercadoLibre/controllers/userController.js
@@ -1,81 +1,77 @@
-const db=require('../database/models');
-const db1=require('../db/data'); //después lo borramos
-let bcrypt=require('bcryptjs');
-const {validationResult} = require("express-validator");
+const db = require("../database/models");
+const db1 = require("../db/data"); //después lo borramos
+let bcrypt = require("bcryptjs");
+const { validationResult } = require("express-validator"); //pido las validaciones de la ruta
+//se usa validationResult como palabra clave, no se puede poner otro nombre
 
-let userController={
-    perfil:function(req,res){
-        return res.render('profile',{
-            data: db.productos,
-            nombreUsuario: db1.usuario[1].email,
-            fotoPerfil: db1.usuario[1].foto_perfil
-        })
-    },
-    editarPerfil:function(req,res){
-        return res.render('profile-edit',{
-            nombreUsuario: db1.usuario[1].email
-        })
-    },
-    registrarse:function(req,res){ //solo sirve para mostrar la vista
-        return res.render('register',{
-            
-        })
-    },
-    iniciarSesion:function(req,res){  //solo sirve para mostrar la vista
-        return res.render('login',{ 
+let userController = {
+  perfil: function (req, res) {
+    return res.render("profile", {
+      data: db.productos,
+      nombreUsuario: db1.usuario[1].email,
+      fotoPerfil: db1.usuario[1].foto_perfil,
+    });
+  },
+  editarPerfil: function (req, res) {
+    return res.render("profile-edit", {
+      nombreUsuario: db1.usuario[1].email,
+    });
+  },
+  registrarse: function (req, res) {
+    //solo sirve para mostrar la vista
+    return res.render("register", {});
+  },
+  iniciarSesion: function (req, res) {
+    //solo sirve para mostrar la vista
+    return res.render("login", {});
+  },
+  register: function (req, res) {
+    
 
+    let errors = validationResult(req);
+    if (errors.isEmpty()) { //una vez que no haya errores, mando los datos a la db
+      let data = req.body;
+      db.Users.create({
+        email: data.email,
+        contrasenia: bcrypt.hashSync(data.contrasenia, 10),
+        fecha: data.fecha_nacimiento,
+        dni: data.nro_documento,
+        foto_perfil: data.foto_perfil,
+        usuario: data.usuario,
+      })
+        .then(function () {
+          return res.redirect("/");
         })
-    },
-    register: function(req,res){
-        let data= req.body
-
-        let errors = validationResult(req);
-
-        if (errors.isEmpty()){
-            
-        }else{
-            res.render("register", {errors: errors.mapped(), old:req.body});
-        }
-        
-        db.Users.create({
-            email: data.email,
-            contrasenia: bcrypt.hashSync(data.contrasenia, 10),
-            fecha: data.fecha_nacimiento,
-            dni: data.nro_documento,
-            foto_perfil: data.foto_perfil,
-            usuario: data.usuario
-        })
-            .then(function(){
-                return res.redirect('/')
-            })
-            .catch(err=>console.log(err))
-    }, 
-    login: function(req,res){
-       let data= req.body
-       
-        db.Users.findOne({ where: { email: data.usuario } })
-        
-            .then(function(user){
-                res.send(user)
-                if (!user){
-                    return res.send("No se encontro");
-                }
-               
-                if (bcrypt.compareSync(data.contrasenia, user.contrasenia)){
-                    if (req.body.recordarme != undefined && data.recordarme){
-                    res.cookie("user", user,  {maxAge: 1000*60*500})
-                    }
-                    return res.redirect('/')   
-                } else{
-                    return res.send("No coinciden contraseñas");
-                }
-            })
-
-        .catch(function(error){
-        console.log(error)
-        })
-       
+        .catch((err) => console.log(err));
+    }else{
+        return res.render("register", {errors: errors.array(), old: req.body}); //en el caso de que haya errores, renderizo de nuevo la vista y los mando.
+                                    //se usa la propiedad errors y old (con old mando todos los datos que ingreso el usuario para que no los ponga de nuevo)
     }
+  },
+  login: function (req, res) {
+    let data = req.body;
 
-}
+    db.Users.findOne({ where: { email: data.usuario } })
+
+      .then(function (user) {
+        res.send(user);
+        if (!user) {
+          return res.send("No se encontro");
+        }
+
+        if (bcrypt.compareSync(data.contrasenia, user.contrasenia)) {
+          if (req.body.recordarme != undefined && data.recordarme) {
+            res.cookie("user", user, { maxAge: 1000 * 60 * 500 });
+          }
+          return res.redirect("/");
+        } else {
+          return res.send("No coinciden contraseñas");
+        }
+      })
+
+      .catch(function (error) {
+        console.log(error);
+      });
+  },
+};
 module.exports = userController;
--- mercadoLibre/routes/user.js
+++ mercadoLibre/routes/user.js
@@ -1,36 +1,41 @@
-const express = require("express")
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 const userController = require("../controllers/userController");
 const productController = require("../controllers/productController");
+const db = require("../database/models");
 
-const { body } = require("express-validator")
+const { body } = require("express-validator"); //para poder validar formularios
+
+//===========  Validaciones  ===============//
+
+let validationsRegister = [
+  body("email")
+    .notEmpty()
+    .withMessage("campo email incompleto") //verifica que el campo no este vacío
+    .isEmail()
+    .withMessage("formato de email incorrecto") //verifica que sea un email válido
+    .custom(function (value, { req }) {
+      //Verifica que el email no exista previamente
+      return db.Users.findOne({
+        where: { email: req.body.email }, //usamos el atributo value del campo como imput
+      }).then(function (user) {
+        if (user) {
+          throw new Error("El email ingresado ya existe.");
+        }
+      });
+    }),
+  body("usuario").notEmpty().withMessage("campo usuario incompleto"),
+  body("contrasenia").notEmpty().withMessage("campo contraseña incompleto"),
+  body("nro_documento").notEmpty().withMessage("DNI incompleto").isLength({ max: 15 }).withMessage("El DNI debe tener como máximo 15 caracteres"),
+];
+
+//============  Rutas  ============//
 
 router.get("/", userController.perfil);
 router.get("/editarPerfil", userController.editarPerfil);
 router.get("/registrarse", userController.registrarse); //mostrar el formulario
-
-let validations = [
-    body("email") 
-        .notEmpty() //verifica que el campo no este vacío
-        .isEmail() //verifica que sea un email válido
-        .custom(function(value, {req}){ //Verifica que el email no exista previamente
-            db.Users.findOne({
-                where: {email: req.body.email}, //usamos el atributo value del campo como imput 
-            })
-            .then(function(user){
-                if(user){
-                    throw new Error("El email ingresado ya existe.");
-                }
-            })
-        })
-]
-
-
-router.post("/newUser", userController.register); //recibe el formulario
+router.post("/newUser", validationsRegister, userController.register); //recibe el formulario
 router.get("/iniciarSesion", userController.iniciarSesion); // mostrar formulario de login
 router.post("/login", userController.login); // recibe formulario de login
-
-
-
 
 module.exports = router;
